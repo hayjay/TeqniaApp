ea79db3f7caca5078729a899bebd2b92cc115c35
Nurudeen Ajayi <ydeveloper15@gmail.com>
2019-11-06 15:37:12 +0100

    StoryBoard/App Setup

---
+++ TeqniaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeqniaApp
+//
+//  Created by Nurudeen Ajayi on 06/11/2019.
+//  Copyright © 2019 Nurudeen Ajayi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
