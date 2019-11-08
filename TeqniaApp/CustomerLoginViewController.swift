f7563e01e07d342c9ebf3b8fce97597c3ad6c1d5
Nurudeen Ajayi <ydeveloper15@gmail.com>
2019-11-09 00:27:05 +0100

    Connected authentication screens together (signup,login,forgot-password)

---
+++ TeqniaApp/CustomerLoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerLoginViewController.swift
+//  TeqniaApp
+//
+//  Created by Nurudeen Ajayi on 09/11/2019.
+//  Copyright © 2019 Nurudeen Ajayi. All rights reserved.
+//
+
+import UIKit
+
+class CustomerLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
